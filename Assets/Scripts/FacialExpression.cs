85f8e63caf2e388a9573c7e7773974d3811f3b6f
Fl4tF33t <89871056+Fl4tF33t@users.noreply.github.com>
2022-11-20 18:34:42 +0200

    facial features

---
+++ Assets/Scripts/FacialExpression.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FacialExpression : MonoBehaviour
+{
+    public Sprite[] facialExpression;
+    AudienceSelect audienceSelectInfo;
+    Image image;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audienceSelectInfo = GetComponentInParent<AudienceSelect>();
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(audienceSelectInfo.leaveLevel == 2)
+        {
+            this.image.sprite = facialExpression[0];
+        }else if (audienceSelectInfo.leaveLevel == 1)
+        {
+            this.image.sprite = facialExpression[1];
+        }else if (audienceSelectInfo.leaveLevel == 0)
+        {
+            this.image.sprite = facialExpression[2];
+        }
+    }
+}
